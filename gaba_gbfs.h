46931925d2eef52e5fc47e093168e88cf9095309
Hajime Suzuki <yline5050hs@gmail.com>
2017-12-02 16:26:02 +0900

    wip

--- gaba_gbfs.h
+++ gaba_gbfs.h
@@ -18,14 +18,13 @@
 
 #include <stdint.h>							/* uint32_t, uint64_t, ... */
 #include "gaba.h"
-#include "sassert.h"
 
 
 /* external (callback) functions */
 typedef link_set_t (*gaba_get_link_t)(void *opaque, uint32_t sid);
-typedef gaba_section_t const *(*gaba_get_segment_t)(uint32_t sid);
-typedef void *(*gaba_get_meta_t)(uint32_t sid);
-typedef void (*gaba_set_meta_t)(uint32_t sid, void *meta);
+typedef gaba_section_t const *(*gaba_get_segment_t)(void *opaque, uint32_t sid);
+typedef void *(*gaba_get_meta_t)(void *opaque, uint32_t sid);
+typedef void (*gaba_set_meta_t)(void *opaque, uint32_t sid, void *meta);
 
 
 
@@ -34,26 +33,35 @@ struct sec_meta_s {
 };
 
 struct tree_path_s {
-	gaba_fill_t const *fill[];
+	struct gaba_fill_s const *fill[];
 
 
 	/* a list of (plim, sec_meta_t) (is it heap-queue?) */
 };
 
-struct link_set_s {
+/**
+ * @struct gaba_gbfs_link_s
+ */
+struct gaba_gbfs_link_s {
 	uint32_t cnt;
-	gaba_section_t const *next;
+	struct gaba_section_s const *sec;
 };
 
-
-struct wfill_s {
-	gaba_fill_t f;
+/**
+ * @struct gaba_gbfs_wfill_s
+ */
+struct gaba_gbfs_wfill_s {
+	struct gaba_fill_s f;
 	/* with plim */
 };
+#define _wfill(x)				( (struct gaba_gbfs_wfill_s *)(x) )
 
-struct qfill_s {
+/**
+ * @struct gaba_gbfs_qfill_s
+ */
+struct gaba_gbfs_qfill_s {
 	int64_t ppos;
-	wfill_t const *f;											/* user-defined metadata after this */
+	struct gaba_joint_tail_s const *tail;				/* user-defined metadata after this */
 };
 
 
@@ -61,47 +69,83 @@ void hq_push(hq_t *hq, qfill_t e) { return; }
 qfill_t hq_pop(hq_t *hq) { return; }
 
 
-#define _is_p_break(_f)			( (_f)->f.stat == CONT )		/* not (UPDATE_A or UPDATE_B) and CONT */
-#define _is_a_break(_f)			( (_f)->f.stat & UPDATE_A )
-#define _is_b_break(_f)			( (_f)->f.stat & UPDATE_B )
+#define _is_p_break(_f)			( (_f)->f.stat == CONT )/* not (UPDATE_A or UPDATE_B) and CONT */
+#define _is_a_break(_f)			( (_f)->f.stat & GABA_UPDATE_A )
+#define _is_b_break(_f)			( (_f)->f.stat & GABA_UPDATE_B )
 
-
-void merge_tails(gdp_t *gdp, wfill_t *f)
+/**
+ * @fn gaba_gbfs_merge_tails
+ */
+void gaba_gbfs_merge_tails(
+	gdp_t *gdp,
+	struct gaba_joint_tail_s *tail)
 {
-	wfill_t **warr = get_meta(f->f);							/* what is the difference between coordinates? */
+	struct gaba_joint_tail_s **warr = get_meta(tail->f);	/* what is the difference between coordinates? */
 	return;
 }
 
-void set_p_break(gdp_t *gdp, wfill_t *f)
+/**
+ * @fn gaba_gbfs_set_break_a, gaba_gbfs_set_break_b
+ * @brief set p-break
+ */
+void gaba_gbfs_set_break_a(
+	gdp_t *gdp,
+	struct gaba_joint_tail_s *tail)
 {
+	tail->abrk |= 0x01ULL<<(BW - 1);
+
+	/* */
+
+	return;
+}
+void gaba_gbfs_set_break_b(
+	gdp_t *gdp,
+	struct gaba_joint_tail_s *tail)
+{
+	tail->bbrk |= 0x01ULL<<(BW - 1);
 	return;
 }
 
-void set_p_break(gdp_t *gdp, wfill_t *f)
+
+/**
+ * @fn gaba_gbfs_extend
+ * @brief traverse graph in a depth-first manner
+ */
+static
+struct gaba_fill_s *gaba_gbfs_extend(
+	gdp_t *gdp,
+	gaba_section_t const *asec,
+	uint32_t apos,
+	gaba_section_t const *bsec,
+	uint32_t bpos)
 {
-	return;
-}
-
-
-/* API */
-gaba_fill_t *fill_graph(gdp_t *gdp, uint64_t aid_pos, uint64_t bid_pos)
-{
-	gaba_section_t const *asec = get_segment(aid_pos>>32);
-	gaba_section_t const *bsec = get_segment(bid_pos>>32);
-
 	/* fill root */ {
-		gaba_fill_t *f = gaba_dp_fill_root(gdp->dp, asec, aid_pos, bsec, bid_pos);
-		hq_push(gdp->q, (qfill_t){ .ppos = f->ppos, .f = (wfill_t *)f });
+		struct gaba_joint_tail_s *tail = _tail(gaba_dp_fill_root(gdp->dp, asec, apos, bsec, bpos));
+		kv_hq_push(gdp->q, (struct gaba_gbfs_qfill_s){
+			.ppos = tail->ppos,							/* sorted by ppos */
+			.tail = tail
+		});
 	}
 
 	while(!hq_is_empty(gdp->q)) {
-		wfill_t *f = hq_pop(gdp->q).f;
-		if(_is_p_break(f)) { merge_tails(gdp, f); }
-		if(_is_a_break(f)) { set_p_break(gdp, f); }
-		if(_is_b_break(f)) { set_p_break(gdp, f); }
-		asec = (f->stat & UPDATE_A) ? get_link(f->a) : f->a;
-		bsec = (f->stat & UPDATE_B) ? get_link(f->b) : f->b;
-		for(asec x bsec) { gdp->q.push(gaba_dp_fill(f, a, b)); }
+		struct gaba_joint_tail_s *tail = kv_hq_pop(gdp->q).tail;/* pop a tail with the shortest path */
+		if(_is_p_break(tail)) { gaba_gbfs_merge_tails(gdp, tail); }
+		if(_is_a_break(tail)) { gaba_gbfs_set_break_a(gdp, tail); }
+		if(_is_b_break(tail)) { gaba_gbfs_set_break_b(gdp, tail); }
+
+		struct gaba_gbfs_link_s alink = ((tail->f.stat & UPDATE_A)
+			? gdp->get_link(gdp->g, tail->s.aid)
+			: (struct gaba_gbfs_link_s){ .cnt = 1, .sec = &tail->a }
+		);
+		struct gaba_gbfs_link_s blink = ((tail->stat & UPDATE_B)
+			? gdp->get_link(gdp->g, tail->s.bid)
+			: (struct gaba_gbfs_link_s){ .cnt = 1, .sec = &tail->b }
+		);
+		for(uint64_t i = 0; i < alink.cnt; i++) {
+			for(uint64_t j = 0; j < blink.cnt; j++) {
+				gdp->q.push(gaba_dp_fill(gdp->dp, f, alink.sec[i], blink.sec[j]));
+			}
+		}
 	}
 
 
