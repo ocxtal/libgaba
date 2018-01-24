de3e0c4b3627658e2ab24b54c5b33bae6e986822
Hajime Suzuki <yline5050hs@gmail.com>
2018-01-24 18:37:04 +0900

    remembered gaba_common.c

---
+++ gaba_common.c
@@ -0,0 +1,19 @@
+
+/**
+ * @file gaba_common.c
+ *
+ * @brief common function implementations
+ *
+ * @author Hajime Suzuki
+ * @date 2018/1/22
+ * @license Apache v2
+ */
+
+#include "gaba.h"
+#include "arch/arch.h"
+#define _GABA_PARSE_EXPORT_LEVEL		/* export them all */
+#include "gaba_parse.h"
+
+/**
+ * end of gaba_common.c
+ */
